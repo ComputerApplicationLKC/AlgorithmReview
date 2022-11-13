4c57f1cc91fc57b90abfc694e50a75ee0920a8f8
ohksj77 <ohksj77@gmail.com>
2022-11-13 19:20:15 +0900

    feat: backend services init

---
+++ apigateway-service/src/test/java/leekimcho/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package leekimcho.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ guest-service/src/test/java/com/leekimcho/guestservice/GuestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leekimcho.guestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
