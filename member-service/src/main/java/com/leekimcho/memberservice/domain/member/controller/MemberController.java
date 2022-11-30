ed8bd20330affca876c786c90130670efe704016
ohksj77 <ohksj77@gmail.com>
2022-11-30 15:16:51 +0900

    feat: member - problem 간 통신 오류 수정중

--- member-service/src/main/java/com/leekimcho/memberservice/domain/member/controller/MemberController.java
+++ member-service/src/main/java/com/leekimcho/memberservice/domain/member/controller/MemberController.java
@@ -1,47 +1,31 @@
 package com.leekimcho.memberservice.domain.member.controller;
 
 import com.leekimcho.memberservice.domain.member.dto.GoogleTokenDto;
-import com.leekimcho.memberservice.domain.member.dto.MemberDto;
-import com.leekimcho.memberservice.domain.member.service.MemberService;
+import com.leekimcho.memberservice.domain.member.entity.Member;
 import com.leekimcho.memberservice.domain.member.service.OauthService;
 import com.leekimcho.memberservice.global.dto.ResponseDto;
-import com.leekimcho.memberservice.global.dto.Result;
 import com.leekimcho.memberservice.global.utils.auth.AuthCheck;
 import com.leekimcho.memberservice.global.utils.auth.MemberContext;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.Column;
 import javax.validation.Valid;
 
 import static com.leekimcho.memberservice.global.advice.message.SuccessMessage.SUCCESS_AUTHORIZATION;
 import static com.leekimcho.memberservice.global.advice.message.SuccessMessage.SUCCESS_ISSUE_TOKEN;
 
-@RestController
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/member-service")
 public class MemberController {
 
-    private final MemberService memberService;
     private final OauthService oauthService;
-
-
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllMembers() {
-        return ResponseEntity.ok(memberService.getAllMembers());
-    }
-
-    @PostMapping("/admin")
-    public ResponseEntity<Void> joinAdmin(@RequestBody MemberDto dto) {
-        memberService.registerAdmin(dto, true);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 
     @AuthCheck
     @GetMapping("/check")
@@ -55,20 +39,20 @@ public class MemberController {
                 HttpStatus.OK, SUCCESS_ISSUE_TOKEN, oauthService.googleLogin(tokenDto.getAccessToken()))
         );
     }
+
     @GetMapping("/member-context")
-    public ResponseEntity<Result<GetMemberResponse>> getMemberContext() {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(Result.createSuccessResult(new GetMemberResponse(new MemberDto(MemberContext.currentMember.get()))));
+    public ResponseEntity<?> getMemberContext() {
+        return ResponseEntity.ok().body(ResponseDto.of(HttpStatus.OK, SUCCESS_AUTHORIZATION, new MemberDto(MemberContext.currentMember.get())));
     }
 
-    @Data @NoArgsConstructor @AllArgsConstructor
-    static class GetMemberResponse {
+    @Getter
+    public static class MemberDto {
         private Long memberId;
         private String nickname;
 
-        public GetMemberResponse(MemberDto memberDto) {
-            this.memberId = memberDto.getId();
-            this.nickname = memberDto.getNickname();
+        public MemberDto(Member member) {
+            this.memberId = member.getId();
+            this.nickname = member.getNickname();
         }
     }
 
--- member-service/src/main/java/com/leekimcho/memberservice/domain/member/dto/JwtPayload.java
+++ member-service/src/main/java/com/leekimcho/memberservice/domain/member/dto/JwtPayload.java
@@ -9,18 +9,7 @@ import java.util.Date;
 public class JwtPayload {
 
     private Long id;
-
     private String email;
-    private String accessToken;
-    private Date accessTokenExpiredDate;
-    private String refreshToken;
-
-    @Builder
-    public JwtPayload(String accessToken, String refreshToken, Date accessTokenExpiredDate) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.accessTokenExpiredDate = accessTokenExpiredDate;
-    }
 
     @Builder
     public JwtPayload(Long id, String email) {
