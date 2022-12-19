1f41445a2c5feda976cdfa9e0a25c840671ecc0f
AshtonJo <hee576@nate.com>
2022-12-19 16:55:04 +0900

    시나리오통합

--- frontend/src/api/get/getProblem.js
+++ frontend/src/api/get/getProblem.js
@@ -1,16 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const base = require('../../utils/base')
+const base = require("../../utils/base");
 
 export const getProblemApi = async (idx) => {
-    const url =
-        base.url + '/problem-service/problems/' + idx;
-    try {
-        const response = await axios.get(`${url}`);
-        return response.data.data;
-    } catch (e) {
-        window.location.href = "/404"
-    }
-}
+  const url = base.url + "/problem-service/problems/" + idx;
+  try {
+    const response = await axios.get(`${url}`);
+    return response.data.data;
+  } catch (e) {
+    window.location.href = "/404";
+  }
+};
 
 export default getProblemApi;
