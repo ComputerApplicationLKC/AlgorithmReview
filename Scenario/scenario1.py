3081ca4fb270037068c097452ebe82852d77d78c
ohksj77 <ohksj77@gmail.com>
2022-12-21 12:55:41 +0900

    feat: 시나리오 1, 2 수정

--- Scenario/scenario1.py
+++ Scenario/scenario1.py
@@ -1,63 +1,46 @@
 from subprocess import Popen, PIPE
 import os
-import threading
 import time
 
-def memory_dynamic(Container_name, upper, mem_unit):
-    updateCommand = "docker update --memory " + "\"" + upper + mem_unit + "\"" + " --memory-swap " + "\"" + upper + mem_unit + "\"" + " " + Container_name
-    print(updateCommand)
-    os.system(updateCommand)
-    return
-
-def memory_unit_conversion(mem_unit):
-    if(mem_unit=="GiB"):
-        mem_unit="gb"
-    if(mem_unit=="MiB"):
-        mem_unit="mb"
-            
-    return mem_unit
-
-
-def extractResourceUsage(out):
-    out = out.split()
-
-    memory = out[-2].decode('utf-8') # memory
-    memory = memory[:-1]
-    cpu = out[2].decode('utf-8') # cpu
-    cpu = cpu[:-1]
-    Container_id = out[0].decode('utf-8') # Container_id
-    Container_name = out[1].decode('utf-8') # Container_name
-    mem_limit = out[5].decode('utf-8') # memory_limit
-    mem_unit = out[5].decode('utf-8')[-3:] # memory_unit
-
-    mem_unit = memory_unit_conversion(mem_unit) # mem_unit 변환
-
-    try:
-        if(memory != ''):
-            if(float(memory) > 80.0): # 자원 할당
-                print(Container_name, 'upper 80%')
-                memory_dynamic(Container_name, str(int(float(mem_limit[:-3]) + 5.0)), mem_unit)
-            elif(float(memory) < 20.0): # 자원 회수
-                print(Container_name, 'under 20%')
-                memory_dynamic(Container_name, str(int(float(mem_limit[:-3]) - 5.0)), mem_unit)
-    except:
-        return
-
-    return
-
-
-
-def run(command):
-    process = Popen(command, stdout=PIPE, shell=True)
+def run(process):
     while True:
         line = process.stdout.readline().rstrip()
         if not line:
             break
         yield line
 
+def getUsage(result):
+    result = result.split()
 
-if __name__ == "__main__":
-    for path in run("docker stats --format \"table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}\""):
-        extractResourceUsage(path)
+    cpu = result[2].decode('utf-8') 
+    cpu = cpu[:-1]
+    Container_name = result[1].decode('utf-8')
+
+    memory = result[-2].decode('utf-8')
+    memory = memory[:-1]
+    mem_limit = result[5].decode('utf-8')
+    mem_unit = result[5].decode('utf-8')[-3:] 
+
+    if 'i' in mem_unit:
+        mem_unit = mem_unit[0] + mem_unit[2]
+        mem_unit.lower()
+
+    if(memory != ''):
+        if(float(memory) > 80.0): 
+            print(Container_name, 'upper 80%')
+            c = "docker update --memory " + "\"" + str(int(float(mem_limit[:-3]) + 4.0)) + \
+                mem_unit + "\"" + " --memory-swap " + "\"" + str(int(float(mem_limit[:-3]) + 4.0)) + mem_unit + "\"" + " " + Container_name
+            os.system(c)
+
+        elif(float(memory) < 20.0): 
+            print(Container_name, 'under 20%')
+            c = "docker update --memory " + "\"" + str(int(float(mem_limit[:-3]) - 4.0)) + \
+                mem_unit + "\"" + " --memory-swap " + "\"" + str(int(float(mem_limit[:-3]) - 4.0)) + mem_unit + "\"" + " " + Container_name
+            os.system(c)
+
+while True:
+    print('Scenario1 running')
+    p = Popen("docker stats --format \"table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}\"", stdout=PIPE, shell=True)
+    for path in run(p):
+        getUsage(path)
         time.sleep(5)
-        # print path (result equals 'docker stats')
--- Scenario/scenario2.py
+++ Scenario/scenario2.py
@@ -6,16 +6,16 @@ import subprocess as sb
 
 
 while(1):
+    print('Scenario2 Running')
     docker_ps = sb.check_output(['docker','ps','-a']).decode()
     docker_ps = [x.strip() for x in docker_ps.split('\n') if x != '']
 
     docker_list = []
     for x in docker_ps[1:]:
         docker_list.append([y.strip() for y in x.split('   ') if y != ''])
+        
+    for con in filter(lambda docker: 'Exited' in docker[4] and docker[5] != 'frontend', docker_list):
+        print(con[5], 'restarted')
+        os.system('docker start ' + con[5])
 
-    for container in docker_list:
-        if 'Exited' in container[4]:
-            if not container[5]=='frontend':
-                print("\ncontainer restart ")
-                os.system('docker start ' + container[5])
     time.sleep(2)
